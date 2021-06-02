9a29656a155389324e61f0b1793ef477a52c09e7
mburnside999 <mburnside@salesforce.com>
2021-06-03 09:24:59 +1000

    test

--- routes/kb.js
+++ routes/kb.js
@@ -13,12 +13,8 @@ conn.login("mburnside@cta5.demo", "salesforce123", function (err, res) {
 
 /* GET home page. */
 router.post("/", function (req, res, next) {
-  //var conn = new jsforce.Connection();
   let response = res;
   let search = req.body.srch;
-
-  //conn.login('mburnside@cta5.demo', 'salesforce123', function(err, res) {
-  //if (err) { return console.error(err); }
   console.log('Searching for articles using search:',search);
 
   conn.search(
@@ -29,35 +25,17 @@ router.post("/", function (req, res, next) {
       if (err) {
         return console.error(err);
       }
-
-      //if (resp.searchRecords.length > 0) {
-        //console.log(JSON.stringify(resp));
-        //let summary = resp.searchRecords[0].Summary;
-        //let ps = resp.searchRecords[0].PublishStatus;
-        //let title = resp.searchRecords[0].Title;
-        //let details = resp.searchRecords[0].Details__c;
-        //let urlname = resp.searchRecords[0].UrlName;
-        //console.log("kbid", resp.searchRecords[0].Id);
         console.log(JSON.stringify(resp.searchRecords));
         response.render("kb", {
           sr: resp.searchRecords
         });
-      //} else response.render("kb", { summary: "No data" });
     }
   );
-  //});
 });
 
 router.get("/article/:kbid", function (req, res, next) {
-  let kbid = req.params["kbid"];
-  console.log("req params", kbid);
   let response = res;
-  //let search=req.body.srch;
-
-  //conn.login("mburnside@cta5.demo", "salesforce123", function (err, res) {
-    //if (err) {
-      //return console.error(err);
-   // }
+  let kbid = req.params["kbid"];
    console.log('Searching for article with kbid:',kbid);
    conn.search('FIND {("*a*") OR ("*e*") OR ("*i*") OR ("*o*") OR ("*u*")} RETURNING Cirrus__kav(UrlName,Id, ArticleType, Details__c,KnowledgeArticleId, PublishStatus,Summary,Title WHERE language=\'en_US\' and Id=\'' +kbid +"')",
       function (err, resp) {
