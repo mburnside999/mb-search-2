2a7fa553657a1689cc9067418b0438c67b98f12d
mburnside999 <mburnside@salesforce.com>
2021-06-02 13:01:15 +1000

    test

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+const cors = require('cors');
+var corsOptions = {
+  origin: 'http://localhost:2000',
+  optionsSuccessStatus: 200 // For legacy browser support
+}
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const fetch = require('node-fetch');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kbRouter = require('./routes/kb');
 
 var app = express();
-
+app.use(cors(corsOptions));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,6 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kb', kbRouter);
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/kb.js
+++ routes/kb.js
@@ -7,21 +7,27 @@ var xx='sssss';
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     var conn = new jsforce.Connection();
-    let fred=res;
+    let response=res;
+    let search=req.body.srch;
+
     conn.login('mburnside@cta5.demo', 'salesforce123', function(err, res) {
       if (err) { return console.error(err); }
-    conn.search("FIND {turbine} RETURNING Cirrus__kav(UrlName,Id, ArticleType, Details__c,KnowledgeArticleId, PublishStatus,Summary,Title WHERE PublishStatus='online')",
+    conn.search("FIND {"+search+"} RETURNING Cirrus__kav(UrlName,Id, ArticleType, Details__c,KnowledgeArticleId, PublishStatus,Summary,Title WHERE PublishStatus='online')",
       function(err, resp) {
         if (err) { return console.error(err); }
-        
-        console.log('xx',xx);
-        console.log('resp',fred);
-        
-        console.log(JSON.stringify(resp.searchRecords));
-        fred.render('kb', { title:'xx'  });
+        if (resp.searchRecords.length>0){
+        console.log(JSON.stringify(resp));
+        var summary=resp.searchRecords[0].Summary;
+        var ps=resp.searchRecords[0].PublishStatus;
+        var title=resp.searchRecords[0].Title;
+        var details=resp.searchRecords[0].Details__c;
+        var urlname=resp.searchRecords[0].UrlName;
 
+        response.render('kb',{'summary':summary,'ps':ps,'title':title,'details':details,'urlname':urlname});
+        } else
+        response.render('kb',{'summary':'No data'});
 
       }
     );
