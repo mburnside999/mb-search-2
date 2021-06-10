7f4fd90de9e0254a3c864a5eb48158f513c1c5b9
mburnside999 <mburnside@salesforce.com>
2021-06-10 14:52:05 +1000

    shared connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const { render } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.redirect('/kbsearch');
 });
 
 module.exports = router;
